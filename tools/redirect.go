0c5839efb71d553a47fcc821e3293bfd489b146f
kshvakov <shvakov@gmail.com>
2015-07-07 10:39:58 +0300

    redirect: add header "Cache-Control" (FF)

--- tools/redirect.go
+++ tools/redirect.go
@@ -6,6 +6,9 @@ import (
 )
 
 func Redirect(c *server.Context, urlStr string, code int) {
+	// FF
+	c.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	c.Header().Set("Expires", "0")
 
 	http.Redirect(c.ResponseWriter, c.Request.HTTP, urlStr, code)
 
